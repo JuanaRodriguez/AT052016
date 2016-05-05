63b2c92636b0a8aeaf470fa5cd92f040193c48cb
JuanaRodriguez <Juana.Rodriguez@fundacion-jala.org>
2016-05-05 12:12:45 -0400

    Practica 2

---
+++ Juana/print_practice_rb.rb
@@ -0,0 +1,7 @@
+print "esta es una prueba de print"
+puts "esta es una prueba de puts"
+print "esta es una prueba de print"
+puts "esta es una prueba de puts"
+print "esta es una prueba de print"
+puts "esta es una prueba de puts"
+print "esta es una prueba de print"
---
+++ Juana/scriptInlineMultiline.rb
@@ -0,0 +1,9 @@
+print "Here beginning the example " # this is an example inline
+=begin
+this is an example of several lines
+this is an example of several lines
+this is an example of several lines
+this is an example of several lines
+this is an example of several lines
+=end
+print "and here finish the example"
