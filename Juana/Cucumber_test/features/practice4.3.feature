9d9c2e7526d8fd2562750dd00127897ca12afab0
JuanaRodriguez <Juana.Rodriguez@fundacion-jala.org>
2016-05-30 18:00:08 -0400

    Practice session 4 4.2

---
+++ Juana/Cucumber_test/features/practice4.3.feature
@@ -0,0 +1,25 @@
+Feature: Gmail account
+
+Scenario: Add gmail account first part
+
+Give I have a name, accepting lower case
+And I have a lastname, accepting lower case
+And I have a username, accepting uppercase and lower case
+And I have a password, accepting any character
+And I have a confirm the password, accepting any character
+And I have a birthday day, accepting only numbers
+And I have a birthday year, accepting only numbers
+And I have a mobile phone, accepting only numbers
+And I have a email address accepting  characteres
+
+When I insert: "juana" in name
+And I insert: "rodriguez" in lastname
+And I insert "Juana_123" in username
+And I insert "abc123" in password
+And I insert "abc123" in confirm the password
+And I insert "6" in birthday day
+And I insert "2016" in birthday year
+And I insert "05415412365" in mobile phone
+And I insert "juana_rodriguez@gmail.com" in email
+
+Then The account should be pass
---
+++ Juana/Cucumber_test/features/step_definitions/step_practice4.3.rb
@@ -0,0 +1,71 @@
+Given(/^I have a lastname, accepting lower case$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a username, accepting uppercase and lower case$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a password, accepting any character$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a confirm the password, accepting any character$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a birthday day, accepting only numbers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a birthday year, accepting only numbers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a mobile phone, accepting only numbers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have a email address accepting  characteres$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert: "([a-zA-Z]*)" in name$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert: "([a-zA-Z]*)" in lastname$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([0-9a-zA-Z_]*)" in username$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([0-9a-zA-Z]*)" in password$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([0-9a-zA-Z]*)" in confirm the password$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([1-30]{1,2}*)" in birthday day$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([1960-2015]{4}*)" in birthday year$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([0-9]{11}*)" in mobile phone$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert "([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3}))" in email$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The account should be pass$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
